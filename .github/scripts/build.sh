9e767a78a8142853edc9f5263a10843221857864
philzy127 <165518581+philzy127@users.noreply.github.com>
2025-12-19 12:10:13 -0500

    Update build.sh
    Updated zip file name to premium

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -25,7 +25,7 @@ echo "Building Pro version..."
 python3 .github/scripts/pro_modifier.py "$PRO_DIR/stackboost-for-supportcandy.php"
 
 cd "$BUILD_DIR/pro"
-zip -r -q ../../stackboost-for-supportcandy-pro.zip .
+zip -r -q ../../stackboost-for-supportcandy-premium.zip .
 cd ../..
 
 # --- BUILD REPO (FREE) VERSION ---
